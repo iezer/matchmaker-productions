b9c9bcfc7f3d1b4c58350a85e5cf003d4c8db411
Isaac Ezer <isaac.ezer@gmail.com>
2023-12-13 15:35:08 +0000

    review updates

--- app/utils/marlow-reviews.js
+++ app/utils/marlow-reviews.js
@@ -1,85 +1,80 @@
 const reviews = [
+  [
+    "The Derek Awards",
+    "★★★★★",
+    "Alternative Shakespeare with rumours, plots, hallucinations and a lot of fun",
+  ],
 
-[
-  "The Derek Awards",
-"★★★★★",
-"Alternative Shakespeare with rumours, plots, hallucinations and a lot of fun"
-],
+  [
+    "EdFest Mag",
+    "★★★★★",
+    "frisky and mischievous",
+    "https://edfestmag.com/the-real-william-shakespeare-as-told-by-christopher-marlowe/",
+  ],
 
+  [
+    "Broadway Baby",
+    "★★★★★",
+    "a vivid little world of meta-theatre",
+    "https://broadwaybaby.com/shows/the-real-william-shakespeare-as-told-by-christopher-marlowe/795771",
+  ],
 
-["EdFest Mag",
-"★★★★★",
-"frisky and mischievous",
-"https://edfestmag.com/the-real-william-shakespeare-as-told-by-christopher-marlowe/"
-],
+  [
+    "Theatre Scotland",
+    "★★★★★",
+    "absolute perfection",
+    "https://theatrescotland.co.uk/2023/08/09/the-real-william-shakespeare-as-told-by-christopher-marlowe-greenside-riddles-court-review-by-stacey-brown/",
+  ],
 
-[
+  [
+    "The Quinntessential Review",
+    "★★★★ ½",
+    "bonkers… a rollicking good time from start to finish",
+    "https://theqr.co.uk/2023/08/06/edfringe-review-the-real-william-shakespeare-as-told-by-christopher-marlowe/",
+  ],
 
-"Broadway Baby",
-"★★★★★",
-"a vivid little world of meta-theatre",
-"https://broadwaybaby.com/shows/the-real-william-shakespeare-as-told-by-christopher-marlowe/795771"
-],
+  [
+    "Corr Blimey",
+    "★★★★",
+    "wonderfully crafted… the past has never been quite so enjoyable",
+    "https://corrblimey.uk/2023/08/13/the-real-william-shakespeare-as-told-by-christopher-marlowe/",
+  ],
 
-[
+  [
+    "All Edinburgh Theatre",
+    "★★★★",
+    "full of imagination… brilliantly performed",
+    "https://www.alledinburghtheatre.com/the-real-william-shakespeare-as-told-by-christopher-marlowe-matchmaker-theatre-productions-edfringe-2023-review/",
+  ],
 
-"Theatre Scotland",
-"★★★★★",
-"absolute perfection",
-"https://theatrescotland.co.uk/2023/08/09/the-real-william-shakespeare-as-told-by-christopher-marlowe-greenside-riddles-court-review-by-stacey-brown/"
-],
+  [
+    "North West End UK",
+    "★★★★",
+    "Matchmaker Theatre Productions have done a splendid job of delivering intrigue… and humour",
+    "https://northwestend.com/the-real-william-shakespeareas-told-by-christopher-marlowe-greenside-at-riddles-court/",
+  ],
 
-[
-"The Quinntessential Review",
-"★★★★ ½",
-"bonkers… a rollicking good time from start to finish",
-"https://theqr.co.uk/2023/08/06/edfringe-review-the-real-william-shakespeare-as-told-by-christopher-marlowe/"
-],
+  [
+    "The Student",
+    "★★★★",
+    "rich in musical and lyrical genius",
+    "https://studentnewspaper.org/fringe-2023-the-real-william-shakespeare-as-told-by-christopher-marlowe/",
+  ],
 
-[
-"North West End UK",
-"★★★★",
-"Matchmaker Theatre Productions have done a splendid job of delivering intrigue… and humour",
-"https://northwestend.com/the-real-william-shakespeareas-told-by-christopher-marlowe-greenside-at-riddles-court/"
-]
+  // Plays International and Europe
+  // EXCELLENT
+  // “highly entertaining”
+  // https://playsinternational.org.uk/the-real-william-shakespeare-as-told-by-christopher-marlowe/
 
+  // Adventures In Theatreland
+  // ★★★★
+  // “charming and amusing… an outlandish comedy”
+  // https://adventuresintheatreland.com/2023/08/11/the-real-william-shakespeare-as-told-by-christopher-marlowe-greenside-ed-fringe/
 
-// Corr Blimey
-// ★★★★
-// “wonderfully crafted… the past has never been quite so enjoyable”
-// https://corrblimey.uk/2023/08/13/the-real-william-shakespeare-as-told-by-christopher-marlowe/ 
-
-
-// The Student
-// ★★★★ 
-// “rich in musical and lyrical genius”
-// https://studentnewspaper.org/fringe-2023-the-real-william-shakespeare-as-told-by-christopher-marlowe/ 
-
-
-// Plays International and Europe
-// EXCELLENT
-// “highly entertaining”
-// https://playsinternational.org.uk/the-real-william-shakespeare-as-told-by-christopher-marlowe/
-
-
-// Adventures In Theatreland
-// ★★★★ 
-// “charming and amusing… an outlandish comedy”
-// https://adventuresintheatreland.com/2023/08/11/the-real-william-shakespeare-as-told-by-christopher-marlowe-greenside-ed-fringe/ 
-
-
-// Adventures In Theatreland
-// ★★★★ 
-// “charming and amusing… an outlandish comedy”
-// https://adventuresintheatreland.com/2023/08/11/the-real-william-shakespeare-as-told-by-christopher-marlowe-greenside-ed-fringe/ 
-
-
-// All Edinburgh Theatre
-// ★★★★ 
-// “full of imagination… brilliantly performed”
-// https://www.alledinburghtheatre.com/the-real-william-shakespeare-as-told-by-christopher-marlowe-matchmaker-theatre-productions-edfringe-2023-review/ 
-
-
+  // Adventures In Theatreland
+  // ★★★★
+  // “charming and amusing… an outlandish comedy”
+  // https://adventuresintheatreland.com/2023/08/11/the-real-william-shakespeare-as-told-by-christopher-marlowe-greenside-ed-fringe/
 ];
 
 export default reviews;
